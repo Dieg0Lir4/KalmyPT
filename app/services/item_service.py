26ab99db1278345c9d6f10f95bf4d9b6b23152ca
VideojuegosDonDiego <a01710369@tec.mx>
2026-02-23 18:27:30 -0600

    feat: crear conexion con repository

---
+++ app/services/item_service.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from app.repositories.item_respository import ItemRepository
+from app.schemas.item import ItemCreate, ItemUpdate
+from fastapi import HTTPException
+
+class ItemService:
+    def __init__(self, db: Session):
+        self.repository = ItemRepository(db)
+
+    def get_all(self, page: int, size: int):
+        items, total = self.repository.get_all(page, size)
+        return {
+            "items": items,
+            "total": total,
+            "page": page,
+            "size": size
+        }
+    
+    def get_by_id(self, item_id: int):
+        item = self.repository.get_by_id(item_id)
+        if not item:
+            raise HTTPException(status_code=404, detail="Item not found")
+        return item
+    
+    def create(self, item: ItemCreate):
+        return self.repository.create(item)
+    
+    def update(self, item_id: int, item: ItemUpdate):
+        self.repository.get_by_id(item_id)
+        return self.repository.update(item_id, item)
+    
+    def delete(self, item_id: int):
+        self.repository.get_by_id(item_id)
+        self.repository.delete(item_id)
